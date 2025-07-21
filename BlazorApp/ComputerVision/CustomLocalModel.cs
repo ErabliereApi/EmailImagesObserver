136c8c8049df0b0f44bfbcfef66545eb4d2fa349
Frédéric Jacques <gary.bob88@gmail.com>
2025-07-20 20:07:26 -0400

    ajust things

--- BlazorApp/ComputerVision/CustomLocalModel.cs
+++ BlazorApp/ComputerVision/CustomLocalModel.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace BlazorApp.ComputerVision;
 
@@ -15,15 +19,27 @@ public class CustomLocalModel
 
     private readonly string[] categories = ["Bassin", "Dompeux", "Séparateur"]; // Remplacez "autre" par votre troisième catégorie
 
-    public string ClassifyImage(byte[] image)
+    public string ClassifyImage(byte[] imageBytes)
     {
         if (_session == null)
-            return "";
+            return "Session not initialized, cannot classify image.";
 
         // Prétraitement de l'image
         var width = 1280;
         var height = 720;
-        var tensor = new DenseTensor<float>(new float[1 * width * height * 3], [1, width, height, 3 ]);
+        var tensor = new DenseTensor<float>(new float[1 * width * height * 3], [1, width, height, 3]);
+
+        var image = Image.Load<Rgb24>(imageBytes);
+        for (var y = 0; y < image.Height; y++)
+        {
+            var pixelSpan = image.GetPixelMemoryGroup()[0].Span.Slice(y * image.Width, image.Width);
+            for (var x = 0; x < image.Width; x++)
+            {
+                tensor[0, 0, y, x] = pixelSpan[x].R;
+                tensor[0, 1, y, x] = pixelSpan[x].G;
+                tensor[0, 2, y, x] = pixelSpan[x].B;
+            }
+        }
 
         // Créer une entrée pour le modèle
         var inputs = new List<NamedOnnxValue>
